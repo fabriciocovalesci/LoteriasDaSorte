8ced99f7ec0aa4c7871b013a5fdc8e95e612e168
fabricio covalesci <fabcovalesci@gmail.com>
2022-01-04 22:28:26 -0300

    add detalhes

--- src/Screens/ListarFiltro/index.js
+++ src/Screens/ListarFiltro/index.js
@@ -48,27 +48,80 @@ export default function ListarFiltro({ route }) {
         getObject()
     }, [getObject])
 
+    const CardListFiltro = ({ item }) => {
+        // nome: nome,
+        // loteria: loteria,
+        // qtadepar: sliderMegaPar,
+        // qtadeimpar: sliderMegaImpar,
+        // qtadedezenas: sliderDezenas,
+        // soma: '0',
+        // maiorocorrencia: maiorOcorrencia,
+        // menorocorrencia: menorOcorrencia,
+        // maioratraso: maiorAtraso,
+        // menoratraso: menorAtraso,
+        // valoraposta: valor,
+        // ultimosconcurso: ultimosconcurso
+
+        function editFiltro(id, loteria){
+            console.log(id, loteria);
+        }
+
+        function deleteFitro(id){
+            console.log(id);
+        }
+
+        function viewFiltro(id){
+            console.log(id);
+        }
+
+
+        const { id, nome, loteria, qtadedezenas } = item;
+        return (
+            <>
+                <View key={id}>
+                    <Card style={{ margin: 5, borderColor: 'blue', borderStyle: "solid", borderWidth: 0.5 }}>
+                        <Card.Content>
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                            <Title numberOfLines={1} ellipsizeMode="tail" style={{ width: 200 }}>{nome}</Title>
+                            { loteria === 'megasena' ? 
+                            <Chip icon="information" style={{ backgroundColor: '#2b6212' }}><Text style={{ color: "#fff" }}>Mega Sena</Text></Chip>
+                            : loteria === 'lotofacil' ?
+                            <Chip icon="information" style={{ backgroundColor: '#930989' }}><Text style={{ color: "#fff" }}>Loto Fácil</Text></Chip>
+                            : loteria === 'lotomania' ?
+                            <Chip icon="information" style={{ backgroundColor: '#F78100' }}><Text style={{ color: "#fff" }}>Loto Mania</Text></Chip>
+                            :  loteria === 'quina' ?
+                            <Chip icon="information" style={{ backgroundColor: '#260085', width: 100 }}><Text style={{ color: "#fff" }}>Quina</Text></Chip>
+                            : null
+                            }
+                            </View>
+                            
+                            <View>
+                                <Text style={{paddingTop: 5, paddingBottom: 5 }}>Quantidade de dezenas: {qtadedezenas}</Text> 
+                            </View>
+        
+                        </Card.Content>
+                        <Card.Actions>
+                            <Button onPress={() => viewFiltro(id)} icon="eye" style={{ borderColor: "blue", margin: 5 }} mode="outlined">Detalhes</Button>
+                            <Button onPress={() => editFiltro(id, loteria)} icon="pencil" style={{ borderColor: "blue", margin: 5 }} mode="outlined">Editar</Button>
+                            <Button onPress={() => deleteFitro(id)} icon="delete-forever-outline" style={{ borderColor: "red", margin: 5 }} mode="outlined">Excluir</Button>
+                        </Card.Actions>
+                    </Card>
+                </View>
+            </>
+        )
+    }
+
     return (
         <>
         <React.Fragment>
             <Provider>
-                <Title style={{ alignSelf: "center", marginTop: 10 }}>Filtros Personalizados</Title>
+                <Title style={{ alignSelf: "center", margin: 10 }}>Filtros Personalizados</Title>
 
-                <Text>{filtros.length}</Text>
-                {/* <View style={{ flex: 1, height: Dimensions.get('screen').height, justifyContent: "center", alignContent: 'center' }}>
-                    <FlatList data={dataLoteria} keyExtractor={item => item.id}
-                        renderItem={CardList} extraData={selectedId}
+                 <View style={{ flex: 1, height: Dimensions.get('screen').height, justifyContent: "center", alignContent: 'center' }}>
+                    <FlatList data={filtros} keyExtractor={item => item.id}
+                        renderItem={CardListFiltro} 
                     />
-                </View> */}
-
-                {/* <Portal>
-                <FAB
-                    style={styles.fab}
-                    small
-                    icon="plus"
-                    onPress={() => alert("criando filtros")}
-                />
-                </Portal> */}
+                </View> 
 
             </Provider>
         </React.Fragment>
